6b9aed26e405da06bcc0606c59accdb6e9c78dcc
mostafizur99 <coder.mr11@gmail.com>
2022-11-29 21:31:09 +0600

    feat: greeting test added

--- src/components/Greetings/Greeting.jsx
+++ src/components/Greetings/Greeting.jsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
+
 const Greeting = () => {
+    const [userJoined, setUserJoined] = useState(false)
+
+    const joinHandler = () => {
+        setUserJoined(!userJoined);
+    }
     return (
         <div className="container mx-auto text-center py-5 bg-zinc-600">
             <h2 className="text-rose-400 text-lg font-bold">Welcome to Nyret Night!</h2>
-            <p className="text-rose-300">The dream night for coders</p>
+            <p className="text-rose-200 italic">The dream night for coders</p>
+
+            <div className="py-5">
+                {!userJoined && <p className="text-rose-300">Join and Get Coding Idea</p>}
+                {userJoined && <p className="text-rose-300">You Are Coder Member</p>}
+                <button onClick={joinHandler} className="mt-5 px-4 py-1.5 bg-rose-300 rounded">
+                    {userJoined ? 'Joined' : 'Join'}
+                </button>
+            </div>
         </div>
     )
 }
--- src/components/Greetings/Greeting.test.js
+++ src/components/Greetings/Greeting.test.js
@@ -1,14 +1,57 @@
 import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting"
 
-test('render Welcome to Nyret Night! as a text', () => {
-    //Arrange
-    render(<Greeting />);
+describe('Greeting component', () => {
+    test('renders "Welcome to Nyret Night!" as a text', () => {
+        //Arrange
+        render(<Greeting />);
 
-    //Act
-    // ...nothing
+        //Act
+        // ...nothing
 
-    // Assert
-    const greetingElement = screen.getByText('Welcome to Nyret Night!');
-    expect(greetingElement).toBeInTheDocument();
-})
+        // Assert
+        const greetingElement = screen.getByText('Welcome to Nyret Night!');
+        expect(greetingElement).toBeInTheDocument();
+    });
+
+    test('renders "Join and Get Coding Idea" if the button was Not clicked', () => {
+        //Arrange
+        render(<Greeting />);
+
+        //Act
+        // ...nothing
+
+        // Assert
+        const joinText = screen.getByText('Join and Get Coding Idea', { exact: false });
+        expect(joinText).toBeInTheDocument();
+    });
+
+    test('renders "You Are Coder Member" if the button was clicked', () => {
+        //Arrange
+        render(<Greeting />);
+
+        //Act
+        const buttonElement = screen.getByRole('button');
+        userEvent.click(buttonElement)
+
+        // Assert
+        const memberText = screen.getByText('You Are Coder Member', { exact: false });
+        expect(memberText).toBeInTheDocument();
+    });
+
+    test('does not render "Join and Get Coding Idea" if the button was clicked', () => {
+        //Arrange
+        render(<Greeting />);
+
+        //Act
+        const buttonElement = screen.getByRole('button');
+        userEvent.click(buttonElement)
+
+        // Assert
+        const joinText = screen.queryByText('Join and Get Coding Idea', { exact: false });
+        expect(joinText).toBeNull();
+    });
+
+});
+
